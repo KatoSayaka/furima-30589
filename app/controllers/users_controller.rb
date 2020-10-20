5d94309d0ff7bdcd1cb4eda88a823e1187ee1af1
KatoSayaka <on85.sayaka.kato@gmail.com>
2020-10-20 17:54:18 +0900

    新規登録・ログイン機能実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  # def neｗ
+  #   User.new(user_params)
+  # end
+
+  # def create
+  #  @user = User.create(user_params)
+  # end
+
+
+  def destroy
+  end
+
+# private
+#   def user_params
+#     params.require(:user).permit:(:nickname, :email, :encrypted_password, :last_name,:first_name, :last_name_kana, :first_name_kana, :birthday)
+#   end
+end
