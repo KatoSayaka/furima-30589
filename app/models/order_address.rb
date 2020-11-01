6de58d594226e078b729cef69071f5912099face
KatoSayaka <on85.sayaka.kato@gmail.com>
2020-11-01 19:23:33 +0900

    単体テストコード

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,11 +6,11 @@ class OrderAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :address_num
-    validates :phone, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+    validates :phone, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid. can't include hyphen(-)."}
     validates :token
   end
 
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank."}
               
                  
     def save
